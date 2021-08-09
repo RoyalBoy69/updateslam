4f2b013ceba2cc4470d9531c0192bef1229376c3
RoyalBoyPriyanshu <64057443+RoyalBoy69@users.noreply.github.com>
2021-08-09 17:41:12 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# # Automatic Updater For slam-mirrorbot
+
+
+![logo](https://telegra.ph/file/db03910496f06094f1f7a.jpg)
+
+
+
+### Steps
+
+##### 1. Clone the repository (if exists, then ignore)
+
+```bash
+git clone https://github.com/RoyalBoy69/updateslam
+```
+
+##### 2. Run the below command to run the script
+
+```bash
+. updateslam/s*
+```
+
+The script will do the process automatically
---
+++ script.sh
@@ -0,0 +1,42 @@
+#Update script of UserBot by @marshmello61
+#Some fixes and imported by @RoyalBoyPriyanshu
+
+repo="slam-tg-mirror-bot"
+rm -rf ${repo}
+
+echo "You're running the slam mirror bot Updater script"
+echo " "
+# Get username of user
+echo -n "Enter your GitHub username: "
+read userName
+echo " "
+
+# Check if user has forked or has the same repo name
+echo -n "Have you forked slam-mirrotbot from breakdowns or has the same repo name i.e. slam-mirrotbot? [y/n]: "
+read fork
+echo " "
+
+if [[ "${fork}" == 'y' ]]; then
+	git clone https://github.com/${userName}/${repo}.git
+	cd ${repo}
+elif [[ "${fork}" == 'n' ]]; then
+	echo -n "Enter your repo name: "
+	read repo
+	git clone https://github.com/${userName}/${repo}.git
+	cd ${repo}
+else
+        echo " "
+        echo "You were only supposed to enter y or n"
+        exit 1
+fi
+
+echo " "
+echo "Updating your slam-mirrorbot"
+git pull https://github.com/breakdowns/slam-tg-mirror-bot.git
+git diff
+git commit -m "updated 🥺"
+git push
+echo " "
+echo "Updated"
+cd ..
+rm -rf ${repo}
